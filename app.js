5a3efb8d84a18fb0bcdf27b66196cd817dcefeb1
athulpm <97athulpm@gmail.com>
2022-11-22 10:22:27 +0530

    Added local storage recovery and delete function

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ var categoryInput = document.getElementById('category');
 var addExpenseBtn = document.getElementById('add-expense');
 var expenseList = document.querySelector('.expense-list');
 
+//get all stored expenses and display it on page
+var expenses = JSON.parse(localStorage.getItem("expenses")) || [];
+expenses.forEach(addElementsAndButtons);
+
 //event listeners
 addExpenseBtn.addEventListener('click', addExpense);
 
@@ -15,31 +19,51 @@ function addExpense(e) {
     const amount = expenseAmountInput.value;
     const description = descriptionInput.value;
     const category = categoryInput.value;
+    
+    const expenseObject = addToArray(amount, description, category);
 
-    const object = {
-        amount: amount,
-        description: description,
-        category: category
-    };
+    localStorage.setItem("expenses", JSON.stringify(expenses));
 
-    localStorage.setItem("expense " + object.amount, JSON.stringify(object));
+    addElementsAndButtons(expenseObject);
+    
+}
 
-    //one expense Item is 1 li with 3 text nodes and 2 buttons
+function addToArray(amount, description, category) {
+    if(amount && description && category)
+    expenses.push({
+        amount,
+        description,
+        category
+    });
+
+    return {amount, description, category};
+}
+
+
+function addElementsAndButtons({amount, description, category}) {
+    //one expense Item is 1 li with 3 <p> tags and 2 buttons
     //create list item
     const expenseLi = document.createElement('li');
     expenseLi.className = "expense-item";
-    //add text node with expense amount to li
-    expenseLi.appendChild(document.createTextNode(object.amount))
-    //add text node with category to li    
-    expenseLi.appendChild(document.createTextNode(` ${object.category}`));
-    //add text node with description to li
-    expenseLi.appendChild(document.createTextNode(` ${object.description}`));
+    //amount text node
+    const amountNode = document.createElement('p');
+    amountNode.innerText = "Amount: " + amount;
+    //description text node
+    const descriptionNode = document.createElement('p');
+    descriptionNode.innerText = "Description: " + description;
+    //category text node
+    const categoryNode = document.createElement('p');
+    categoryNode.innerText = "Category: " + category;
+    //append to expenseLI
+    expenseLi.append(amountNode, descriptionNode, categoryNode);
+    
     
     //make and append delete button
     const deleteBtn = document.createElement('button');
     deleteBtn.className = "delete-expense";
     deleteBtn.innerText = "Delete";
     expenseLi.appendChild(deleteBtn);
+    deleteBtn.addEventListener('click', deleteEle);
 
     //make and append edit button
     const editBtn = document.createElement('button');
@@ -50,6 +74,13 @@ function addExpense(e) {
     //append li to expenseList ul
     expenseList.appendChild(expenseLi)
     
+}
+
+function deleteEle(e) {
+    console.log(expenses);
+    const btn = e.target.parentNode;
+    console.log(btn)
+    btn.remove();
 }
 
 
